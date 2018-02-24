1f67904eeead7fb076b9c5d384ce5ebe909145d8
FelixGehm <felixgehm@googlemail.com>
2018-02-24 22:03:43 +0100

    Trennung Animations Sync von CharacterAnimator

---
+++ Hero Defense/Assets/NetworkSyncAnimations.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class NetworkSyncAnimations : NetworkBehaviour
+{
+    [SyncVar]
+    float speedPercent;
+
+    CharacterAnimator characterAnimator;
+
+    private void Start()
+    {
+        characterAnimator = GetComponent<CharacterAnimator>();
+
+        if (!isLocalPlayer)
+            characterAnimator.isMovedByAgent = false;
+    }
+
+    void Update()
+    {
+        if (isLocalPlayer)
+        {
+            speedPercent = characterAnimator.GetSpeedPercent();
+            CmdSetSpeedPercent(characterAnimator.GetSpeedPercent());
+        }
+        else
+        {
+            characterAnimator.SetSpeedPercent(speedPercent);
+        }
+    }
+
+    [Command]
+    void CmdSetSpeedPercent(float speed)
+    {
+        characterAnimator.SetSpeedPercent(speed);
+    }
+
+}
