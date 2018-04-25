f15dafbff946c13e2eeb91d495c169d3943b8644
Florian Jann <florian.jann@hotmail.de>
2018-04-25 14:50:45 +0200

    Gunslinger Q nerly done

---
+++ Hero Defense/Assets/Scripts/Abilities/Gunslinger/AbilityGunslingerQ.cs
@@ -0,0 +1,183 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class AbilityGunslingerQ : NetworkBehaviour
+{
+    private Camera cam;
+
+    public float abilityCooldown = 4.0f;
+    private float currentCooldown = 0.0f;
+
+    [Space]
+    public GameObject previewPrefab;
+    private GameObject previewGameObject;
+    
+    [Header("Projectile settings")]
+    public GameObject projectilePrefab;
+
+    public float projectileSpeed = 10.0f;   
+
+    public float projectilePhysicalDamage = 100.0f;
+
+    
+
+    private bool isCasting = false;
+   
+
+
+    // Use this for initialization
+    void Start()
+    {
+        GetComponent<CharacterEventManager>().OnAbilityOne += Cast;        
+        cam = Camera.main;
+
+        
+
+        if(previewPrefab == null)
+        {
+            Debug.LogWarning("No Preview-Prefab on AbilityGunslingerQ!");
+        }
+
+        if (projectilePrefab == null)
+        {
+            Debug.LogWarning("No Projectile-Prefab on AbilityGunslingerQ!");
+        }
+    }
+
+    void Update()
+    {
+        if(!isCasting)
+        {
+            currentCooldown -= Time.deltaTime;
+        }
+        else
+        {
+            if (previewGameObject != null)
+            {
+                // Position und Rotation der Preview anpassen
+                previewGameObject.transform.position = transform.position;
+                previewGameObject.transform.rotation = Quaternion.AngleAxis(GetAngleFromDirection(), Vector3.up);
+            }
+
+            if (Input.GetMouseButtonDown(1))        // RightClick
+            {
+                CancelCast();
+            }
+
+            if (Input.GetMouseButtonDown(0))        // LeftClick
+            {
+                ShootProjectile( GetDirectionVectorBetweenPlayerAndMouse() );
+                isCasting = false;
+                GetComponent<PlayerController>().isCasting = false;
+                Destroy(previewGameObject);
+            }
+        }
+    }
+
+    public void Cast()
+    {
+        
+        if (currentCooldown <= 0)
+        {
+            //Debug.Log("Cast(): GunslingerQ");
+            GetComponent<PlayerController>().isCasting = true;
+            ShowPreview();            
+        }
+    }
+
+    public void CancelCast()
+    {
+        isCasting = false;
+        GetComponent<PlayerController>().isCasting = false;
+
+        Destroy(previewGameObject);
+    }
+
+
+    private void ShowPreview()
+    {
+        isCasting = true;
+
+        previewGameObject = Instantiate(previewPrefab);
+    }
+
+    private void ShootProjectile(Vector3 direction)
+    {
+     //   Debug.Log("ShootProjectile():");
+
+        currentCooldown = abilityCooldown;
+        
+        if(isServer)
+        {
+            CmdSpawnProjectileOnServer(projectilePhysicalDamage,direction);
+        } else
+        {
+            TellServerToSpawnProjectile(projectilePhysicalDamage, direction);
+        }
+        
+    }
+
+    private Vector3 GetDirectionVectorBetweenPlayerAndMouse()
+    {
+        Vector3 playerPos = transform.position;
+
+        Vector3 mousePos = new Vector3();
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 10000))
+        {
+            mousePos = hit.point;
+        }
+
+        Vector3 direction = Vector3.Normalize(mousePos - playerPos);
+        
+        return direction;
+    }
+
+    private float GetAngleFromDirection()
+    {
+        float angle = 0.0f;
+        Vector3 direction = GetDirectionVectorBetweenPlayerAndMouse();
+        angle = Vector3.Angle(Vector3.forward, direction);
+
+        if (direction.x < 0.0f)
+        {
+            angle = 360.0f-angle;
+        }
+        //Debug.Log("angle = " + angle);
+        return angle;
+    }
+
+    [Command]
+    void CmdSpawnProjectileOnServer(float damage, Vector3 direction)
+    {
+
+        Vector3 firePoint = previewGameObject.GetComponent<CalcDistanceFromStartToEnd>().GetStartPos();
+
+        GameObject projectileGO = (GameObject)Instantiate(projectilePrefab, firePoint, previewGameObject.transform.rotation);
+        GunslingerQProjectile projectile = projectileGO.GetComponent<GunslingerQProjectile>();
+
+        if (projectile != null)
+        {             
+            projectile.SetDirection(direction);
+
+            float maxDistance = previewGameObject.GetComponent<CalcDistanceFromStartToEnd>().GetDistance();
+            
+            projectile.SetMaxDistance(maxDistance);
+            projectile.speed = projectileSpeed;
+            projectile.damage = damage;
+        }
+
+        //Debug.Log(projectileGO);
+        NetworkServer.Spawn(projectileGO);
+    }
+
+    [ClientCallback]
+    void TellServerToSpawnProjectile(float damage, Vector3 direction)
+    {
+        if (!isServer)
+        {
+            CmdSpawnProjectileOnServer(damage, direction);
+        }
+    }
+}
