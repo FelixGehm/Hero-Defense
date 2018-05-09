399f8ed6f24a2654f7b2c07be6047186e0e39048
Moritz <moritz.thiele@stud.fh-flensburg.de>
2018-05-09 14:03:04 +0200

    Moritz Szene

---
+++ Hero Defense/Assets/Scripts/HUDSimulator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUDSimulator : MonoBehaviour {
+
+
+
+	public bool isImgOn;
+	public Image img;
+
+	void Start () {
+
+		img.enabled = true;
+		isImgOn = true;
+	}
+
+	void Update () {
+
+		if (Input.GetKeyDown ("i")) {
+
+			if (isImgOn == true) {
+
+				img.enabled = false;
+				isImgOn = false;
+			}
+
+			else {
+
+				img.enabled = true;
+				isImgOn = true;
+			}
+		}
+	}
+}
